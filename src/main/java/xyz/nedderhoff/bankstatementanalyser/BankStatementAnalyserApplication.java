8af79ee57d7b2fd88272d027d9750587200f065d
ynedderhoff <hey@yanick.xyz>
2020-10-18 15:15:02 +0200

    add basic Spring Boot app

---
+++ src/main/java/xyz/nedderhoff/bankstatementanalyser/BankStatementAnalyserApplication.java
@@ -0,0 +1,13 @@
+package xyz.nedderhoff.bankstatementanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/nedderhoff/bankstatementanalyser/BankStatementAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nedderhoff.bankstatementanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStatementAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
