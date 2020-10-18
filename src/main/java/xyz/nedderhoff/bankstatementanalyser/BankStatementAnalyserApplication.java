d3317c91a9d679413bca01233e15be8efa05e398
ynedderhoff <hey@yanick.xyz>
2020-10-18 17:12:03 +0200

    add pdf up- and downloading code

--- src/main/java/xyz/nedderhoff/bankstatementanalyser/BankStatementAnalyserApplication.java
+++ src/main/java/xyz/nedderhoff/bankstatementanalyser/BankStatementAnalyserApplication.java
@@ -2,8 +2,10 @@ package xyz.nedderhoff.bankstatementanalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class BankStatementAnalyserApplication {
 
 	public static void main(String[] args) {
